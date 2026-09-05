9d49a5d8f4ddece002c1670c7c3c2c32bb979b92
Manraj Singh <manrajsinghatwal123@gmail.com>
2026-09-05 11:01:35 +0530

    Add files via upload

--- mini_game.py
+++ mini_game.py
@@ -1,4 +1,15 @@
 # A mini number guessing name:
 import random
 secret_number=random.randint(1,10)
-num=int(input("WHAT'S YOUR BEST GUESS:"))
+guess=int(input("WHAT'S YOUR BEST GUESS:"))
+while guess!=secret_number:
+    if(guess<secret_number):
+        print("TOO LOW")
+        break
+    elif(guess>secret_number):
+        print("TOO HIGH")
+        break
+    elif(guess==secret_number):
+        print("CORRECT THE ANSWER IS:",secret_number)
+        break
+print("THE CORRECT ANSWER WAS:",secret_number)
