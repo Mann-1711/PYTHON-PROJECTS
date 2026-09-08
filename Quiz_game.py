4e161981d498a22673893861b34e75aac2f1f11b
Manraj Singh <manrajsinghatwal123@gmail.com>
2026-09-08 20:58:13 +0530

    Add files via upload

---
+++ Quiz_game.py
@@ -0,0 +1,8 @@
+question=("what is the capital of India ?",
+          "what is 10+7= ?",
+          "Who is best football player of all time ?",
+          "In which state of India do people speak Bengali ?",
+          "Which country have  the highest population in the world ?")
+answers=("delhi","17","cristiano ronaldo","west bengal","india")
+ianswer=input("Enter your correct answer:")
+i=0
