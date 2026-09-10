32e39d04e9e60139bcf7bb63e2bda1bf48644699
Manraj Singh <manrajsinghatwal123@gmail.com>
2026-09-10 19:41:21 +0530

    Add files via upload

---
+++ shooping_bill.py
@@ -0,0 +1,9 @@
+#This is a small program to enter differnt items and calculate the final price:
+items={"bread","choclates","eggs","vegetables"}
+price={50,30,40,60}
+i=0
+while i<4:
+    a=input("Which items do you want ?")
+    b=input("quantity of items:")
+    i+=1
+    
